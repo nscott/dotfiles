1c89cc100aff7c0de5713220615f8a8219921892
Nathan Scott <nscott@rjmetrics.com>
2014-05-29 10:21:47 -0400

    Simple script to get running faster on a fresh vm

---
+++ copy_over_vm.sh
@@ -0,0 +1,6 @@
+sudo cp .bash_ps1 ~/.bash_ps1
+cp .emacs_console ~/.emacs
+cp .gitconfig ~/.gitconfig
+cp .siegerc ~/.siegerc
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+cp .zshrc_vm ~/.zshrc
