2eaeab44ccf3f976cdc7a21b1c457e2eb9a0c255
Nathan Scott <natedscott@gmail.com>
2014-05-29 11:43:11 -0400

    installs all the packages

--- copy_over_vm.sh
+++ copy_over_vm.sh
@@ -2,5 +2,6 @@ sudo cp .bash_ps1 ~/.bash_ps1
 cp .emacs_console ~/.emacs
 cp .gitconfig ~/.gitconfig
 cp .siegerc ~/.siegerc
+sudo apt-get install zsh fortune cowsay -y
 wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 cp .zshrc_vm ~/.zshrc
