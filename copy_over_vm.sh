87d7df7e13e0cf1772a47188538b542e81f66ab3
Nathan Scott <nscott@rjmetrics.com>
2014-06-09 10:53:51 -0400

    Modified the copy script a little

--- copy_over_vm.sh
+++ copy_over_vm.sh
@@ -4,5 +4,7 @@ cp .gitconfig ~/.gitconfig
 cp .siegerc ~/.siegerc
 mkdir ~/.emacs.d/el
 sudo apt-get install zsh fortune cowsay -y
-wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+sudo wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+chsh -s /bin/zsh
+sudo chsh -s /bin/zsh
 cp .zshrc_vm ~/.zshrc
